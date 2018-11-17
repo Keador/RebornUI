dcf3e98c79339d4c68f8d3adcfb5c34ba062eb41
keador <keador80@gmail.com>
2018-11-17 03:33:10 -0600

    -- added shared media

---
+++ media/SharedMedia.lua
@@ -0,0 +1,6 @@
+local LSM = RebornUI.LSM;
+
+LSM:Register("statusbar","RUI Blue", [[Interface\AddOns\RebornUI\media\textures\statusBars\blueGradient.tga]]);
+LSM:Register("statusbar","RUI Light Blue", [[Interface\AddOns\RebornUI\media\textures\statusBars\lightBlueGradient.tga]]);
+LSM:Register("statusbar","RUI Dark Blue", [[Interface\AddOns\RebornUI\media\textures\statusBars\darkBlueGradient.tga]]);
+LSM:Register("statusbar","RUI Gray", [[Interface\AddOns\RebornUI\media\textures\statusBars\grayGradient.tga]]);
